ac084c1d1fc17008ca03e86d2f4eeb535697ece6
ahmed-abdat <ahmedeabdat@gmail.com>
2024-11-16 02:19:00 +0100

    updat the next config and fix the logo position

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,5 @@ const nextConfig = {
   },
 };
 
-// Add this condition for development environment
-if (process.env.NODE_ENV === 'development') {
-  nextConfig.metadata = {
-    ...nextConfig.metadata,
-    metadataBase: new URL('http://localhost:3000'),
-  };
-}
 
 export default nextConfig;
