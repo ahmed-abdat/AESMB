e21a802d304c8762559a55964fd01695db6be1be
ahmed-abdat <ahmedeabdat@gmail.com>
2024-11-16 02:14:54 +0100

    updat the next config and layout to use logo as opengraph image

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["tenor.com", "firebasestorage.googleapis.com"],
   },
 };
+
+// Add this condition for development environment
+if (process.env.NODE_ENV === 'development') {
+  nextConfig.metadata = {
+    ...nextConfig.metadata,
+    metadataBase: new URL('http://localhost:3000'),
+  };
+}
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,32 @@ import type { Metadata } from "next";
 import { Header } from "@/components/Header";
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://aesmb.vercel.app/'),
   title: 'Ligue AESMB - Championnat de Football',
   description: 'Suivez le championnat de football AESMB. Classements en direct, gestion des équipes et calendrier des matchs.',
   keywords: 'AESMB, football, championnat, tournoi, classement, matchs',
   authors: [{ name: 'AESMB' }],
   openGraph: {
-    title: 'Ligue AESMB - Championnat de Football',
-    description: 'Suivez le championnat de football AESMB. Classements en direct, gestion des équipes et calendrier des matchs.',
     type: 'website',
     locale: 'fr_FR',
+    url: '/',
+    title: 'Ligue AESMB - Championnat de Football',
+    description: 'Suivez le championnat de football AESMB. Classements en direct, gestion des équipes et calendrier des matchs.',
+    siteName: 'Ligue AESMB',
+    images: [
+      {
+        url: '/logo.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Ligue AESMB Logo',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ligue AESMB - Championnat de Football',
+    description: 'Suivez le championnat de football AESMB. Classements en direct, gestion des équipes et calendrier des matchs.',
+    images: ['/logo.jpg'],
   },
 };
 
