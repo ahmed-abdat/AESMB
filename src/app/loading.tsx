10266df7c3834a5b476eb1790f693011582f6001
ahmed-abdat <ahmedeabdat@gmail.com>
2024-11-24 02:39:14 +0100

    updating the loading.tsx to match my design

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,13 @@
 export default function Loader() {
-    return (
-      <div className="loader">
-      <div className="spinner"></div>
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-background">
+      <div className="loading-container">
+        <div className="loading-spinner">
+          <div className="loading-ball"></div>
+        </div>
+        <p className="loading-text">Chargement...</p>
+      </div>
     </div>
-    )
-  }
+  );
+}
   
